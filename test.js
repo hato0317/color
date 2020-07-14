ddd1e3725c1c38acd9b63397de892122fe6b5642
hato0317 <52101255+hato0317@users.noreply.github.com>
2020-07-14 15:46:51 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,32 +1,32 @@
-$(function(){
-    $('a[href^="#"]').click(function(){
+jQuery(function(){
+    jQuery('a[href^="#"]').click(function(){
       var speed = 400;
-      var href = $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
+      var href = jQuery(this).attr("href");
+      var target = jQuery(href == "#" || href == "" ? 'html' : href);
       var position = target.offset().top -50;
-      $('body,html').animate({scrollTop:position},speed,'swing');
+      jQuery('body,html').animate({scrollTop:position},speed,'swing');
       return false;
     });
   });
-  $(function(){
-    $('#labo-button').click(function(){
-      var colorGroup = $('#children,#female,#adult');
-      var colorButton = $('#children-button,#female-button,#adult-button');
-      $(colorGroup).fadeOut();
-      $('#labo').hide().fadeIn();
-      $(colorButton).fadeOut();
-      $('.colorTitle').fadeOut();
+  jQuery(function(){
+    jQuery('#labo-button').click(function(){
+      var colorGroup = jQuery('#children,#female,#adult');
+      var colorButton = jQuery('#children-button,#female-button,#adult-button');
+      jQuery(colorGroup).fadeOut();
+      jQuery('#labo').hide().fadeIn();
+      jQuery(colorButton).fadeOut();
+      jQuery('.colorTitle').fadeOut();
     });
   });
-  $(function(){
+  jQuery(function(){
     var colorGroup = $('#children,#female,#adult');
-    $('#colors').click(function(){
-      $('#children').hide().fadeIn();
-      $('#labo').fadeOut();
-      $(colorButton).fadeIn();
+    jQuery('#colors').click(function(){
+      jQuery('#children').hide().fadeIn();
+      jQuery('#labo').fadeOut();
+      jQuery(colorButton).fadeIn();
     });
   });
-  $(function(){
-    $('#labo').hide();
+  jQuery(function(){
+    jQuery('#labo').hide();
   });
   
